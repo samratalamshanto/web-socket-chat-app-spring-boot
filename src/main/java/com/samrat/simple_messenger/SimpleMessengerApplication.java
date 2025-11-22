c594a0689d53b991448dc3d60f64cc81cbf38986
Samrat Alam <samratalamshanto710@gmail.com>
2025-11-22 19:20:42 +0600

    initial commit

---
+++ src/main/java/com/samrat/simple_messenger/SimpleMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.samrat.simple_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMessengerApplication.class, args);
+	}
+
+}
